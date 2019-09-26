264b49a527e1861d9bd0f6fe1a0b2647b0a7df74
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2019-09-26 16:59:06 +1200

    Code refactoring

--- system/error.go
+++ system/error.go
@@ -3,6 +3,28 @@ package errors
 
 import "fmt"
 
+// ArgumentNilError indicates that the provided input argument is invalid.
+type ArgumentNilError struct {
+	ArgumentName string
+	ErrorMessage string
+	message      string
+}
+
+// Error returns message for the TenantAlreadyExistsError error type
+// Returns the error nessage
+func (e ArgumentNilError) Error() string {
+	return e.message
+}
+
+// NewArgumentNilError creates a new ArgumentNilError error
+func NewArgumentNilError(argumentName, errorMessage string) error {
+	return ArgumentNilError{
+		ArgumentName: argumentName,
+		ErrorMessage: errorMessage,
+		message:      fmt.Sprintf("Argument \"%s\" is nil. Error message: %s", argumentName, errorMessage),
+	}
+}
+
 // ArgumentError indicates that the provided input argument is invalid.
 type ArgumentError struct {
 	ArgumentName string
